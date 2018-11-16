117a84e2d43ca70d9f85de44d948d223570a4657
nguyenju <nguyenju@plu.edu>
2018-11-16 13:55:51 -0800

    Added query to get list of clubs to SQL commands.

--- Server/src/main/java/TestJDBCConnection.java
+++ Server/src/main/java/TestJDBCConnection.java
@@ -13,7 +13,7 @@ public class TestJDBCConnection {
     public static void connect(){
         try{
             //Create database connection    //This url is where the file ClubDatabase is located. Mine is in my downloads folder.
-            String url = "jdbc:sqlite:/C:/Users/JimmyTN/Downloads/ClubDatabase";
+            String url = "jdbc:sqlite:/D:/SQLServer/ClubDatabase";
             conn = DriverManager.getConnection(url);
 
             System.out.println("Connection to SQLite has been established");
@@ -38,6 +38,11 @@ public class TestJDBCConnection {
         connect();
     }
 
+    /**
+     * Queries the database for a ResultSet of all users from a given club
+     * @param clubName The Club that users belong to
+     * @return Returns Names of Users from a given Club
+     */
     public static ResultSet getUsersFromClub(String clubName){
         ResultSet rs = null;
         String sql = null;
@@ -50,7 +55,25 @@ public class TestJDBCConnection {
                     "ORDER BY u.Email asc";
             rs = stmt.executeQuery(sql);
         } catch (SQLException e) {
-            System.out.println("GetUsesFromClubError " + e.getMessage() + sql);
+            System.out.println("GetUsersFromClubError " + e.getMessage() + sql);
+        }
+        return rs;
+    }
+
+    /**
+     * Queries and returns a ResultSet of all clubs from the database
+     * @return Returns all data for clubs
+     */
+    public static ResultSet getClubs(){
+        ResultSet rs = null;
+        String sql = null;
+
+        try{
+            Statement stmt = conn.createStatement();
+            sql = "SELECT * FROM Clubs";
+            rs = stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            System.out.println("GetClubs Error " + e.getMessage() + sql);
         }
         return rs;
     }
