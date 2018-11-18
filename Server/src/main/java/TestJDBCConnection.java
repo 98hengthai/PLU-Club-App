477dc08935a223e42daffc4f3dafcf412a392434
nguyenju <nguyenju@plu.edu>
2018-11-17 22:51:22 -0800

    Added queries to support club list generation. WIP

--- Server/src/main/java/TestJDBCConnection.java
+++ Server/src/main/java/TestJDBCConnection.java
@@ -64,16 +64,133 @@ public class TestJDBCConnection {
      * Queries and returns a ResultSet of all clubs from the database
      * @return Returns all data for clubs
      */
-    public static ResultSet getClubs(){
+    public static ResultSet getAllClubs(){
         ResultSet rs = null;
         String sql = null;
 
         try{
             Statement stmt = conn.createStatement();
-            sql = "SELECT * FROM Clubs";
+            sql = "SELECT * " +
+                    "FROM Clubs " +
+                    "ORDER BY Name ";
             rs = stmt.executeQuery(sql);
         } catch (SQLException e) {
             System.out.println("GetClubs Error " + e.getMessage() + sql);
+        }
+        return rs;
+    }
+
+    /**
+     * Queries and returns a ResultSet of all clubs from database
+     *  that match or contain the name
+     * @param name
+     * @return
+     */
+    public static ResultSet getAllClubsNameSearch(String name){
+        ResultSet rs = null;
+        String sql = null;
+
+        try{
+            Statement stmt = conn.createStatement();
+            sql = "SELECT * " +
+                    "FROM Clubs " +
+                    "WHERE Name LIKE "  + name + " " +
+                    "ORDER BY Name ";
+        } catch (SQLException e) {
+            System.out.println("GetAllClubsNameSearch error " + e.getMessage() + sql);
+        }
+        return rs;
+    }
+
+    /**
+     * Queries and returns a ResultSet of all clubs that relate to a given interest
+     * @param interest
+     * @return
+     */
+    public static ResultSet getAllClubsIntSearch(String interest){
+        ResultSet rs = null;
+        String sql = null;
+
+        try{
+            Statement stmt = conn.createStatement();
+            sql = "SELECT * " +
+                    "FROM Clubs as c join ClubInterests as ci " +
+                    "ON c.Name = ci.ClubName " +
+                        "AND ci.InterestName = " + interest + " " +
+                    "ORDER BY Name ";
+        } catch (SQLException e) {
+            System.out.println("GetAllClubsIntSearch error " + e.getMessage() + sql);
+        }
+        return rs;
+    }
+
+    /**
+     * Queries and returns a ResultSet of my clubs from the database given a Username
+     * @param userName
+     * @return
+     */
+    public static ResultSet getMyClubs(String userName){
+        ResultSet rs = null;
+        String sql = null;
+
+        try{
+            Statement stmt = conn.createStatement();
+            sql = "SELECT * " +
+                    "FROM Clubs " +
+                    "WHERE Name = ( " +
+                            "SELECT ClubName " +
+                            "FROM ClubUsers " +
+                            "WHERE UserEmail =  (SELECT Email FROM Users WHERE Name = " + userName + " ) )";
+        } catch (SQLException e) {
+            System.out.println("GetMyClubs error " + e.getMessage() + sql);
+        }
+        return rs;
+    }
+
+    //TODO
+    public static ResultSet getMyClubsNameSearch(String name){
+        ResultSet rs = null;
+        String sql = null;
+
+        try{
+            Statement stmt = conn.createStatement();
+            sql = "";
+        } catch (SQLException e) {
+            System.out.println("GetMyClubsNameSearch error " + e.getMessage() + sql);
+        }
+        return rs;
+    }
+
+    //TODO
+    public static ResultSet getMyClubsIntSearch(String interest){
+        ResultSet rs = null;
+        String sql = null;
+
+        try{
+            Statement stmt = conn.createStatement();
+            sql = "";
+        } catch (SQLException e) {
+            System.out.println("GetMyClubsIntSearch error " + e.getMessage() + sql);
+        }
+        return rs;
+    }
+
+    /**
+     * TODO
+     * Given a username, return a list of clubs that match that
+     *  User's interests and order it by the number of interests matched
+     * @param userName
+     * @return A ResultSet of club names that match User Interests
+     */
+    public static ResultSet getRecomClubs(String userName){
+        ResultSet rs = null;
+        String sql = null;
+
+        try{
+            Statement stmt = conn.createStatement();
+            sql = "";
+        } catch (SQLException e) {
+            System.out.println("GetAllClubsNameSearch error " + e.getMessage() + sql);
         }
         return rs;
     }
