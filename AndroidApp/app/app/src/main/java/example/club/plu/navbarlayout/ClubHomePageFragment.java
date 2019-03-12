498dff17b4b79cdf9c606f6c94a12cf80594c4e9
98hengthai <liv.hengthai@gmail.com>
2019-03-12 02:40:28 -0700

    Create ClubHomePageFragment.java
    Clubs homepage fragment links with fragment_clubhomepage.xml file

---
+++ AndroidApp/app/app/src/main/java/example/club/plu/navbarlayout/ClubHomePageFragment.java
@@ -0,0 +1,19 @@
+package example.club.plu.navbarlayout;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ClubHomePageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.content_main, container, false);
+
+        return view;
+    }
+}
