ad74d7dc4abbe81acd1ff85c9b17f4457d33db86
98hengthai <liv.hengthai@gmail.com>
2019-03-12 02:42:43 -0700

    ClubsFragment got private var(s)
    Make a array for all clubs name and a method to init the array. On clicked club, open the club fragment.

--- AndroidApp/app/app/src/main/java/example/club/plu/navbarlayout/ClubsFragment.java
+++ AndroidApp/app/app/src/main/java/example/club/plu/navbarlayout/ClubsFragment.java
@@ -1,5 +1,6 @@
 package example.club.plu.navbarlayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,25 +16,32 @@ import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ClubsFragment extends Fragment {
+    private String[] clubsArry;
     private ListView clubsListView;
     private SearchView searchView;
-    String[] clubs = {"AbsoLUTE: Christian Fellowship","Anime Club","Art Club","Chemistry Club","Dance Club","Health Sciences Club","Math Club",
-            "AbsoLUTE: Christian Fellowship","Anime Club","Art Club","Chemistry Club","Dance Club","Health Sciences Club","Math Club"};
 
+//    clubsArry = new String[]{"AbsoLUTE: Christian Fellowship","Anime Club","Art Club","Chemistry Club","Dance Club","Health Sciences Club","Math Club",
+//            "AbsoLUTE: Christian Fellowship","Anime Club","Art Club","Chemistry Club","Dance Club","Health Sciences Club","Math Club"};
+
+
+    public void initializeClubsArray(String[] clubs){
+//       clubsArry = clubs;
+        clubsArry = new String[]{"AbsoLUTE: Christian Fellowship","Anime Club","Art Club","Chemistry Club","Dance Club","Health Sciences Club","Math Club",
+                "AbsoLUTE: Christian Fellowship","Anime Club","Art Club","Chemistry Club","Dance Club","Health Sciences Club","Math Club"};
+    }
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_clubs, container, false);
-
-        Arrays.sort(clubs);
         ListView clubsListView = view.findViewById(R.id.clubs_listView);
         searchView = (SearchView) view.findViewById(R.id.clubs_searchView);
 
-        final ListAdapter clubsListViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_expandable_list_item_1, clubs);
+        final ListAdapter clubsListViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_expandable_list_item_1, clubsArry);
         clubsListView.setAdapter(clubsListViewAdapter);
         clubsListView.setTextFilterEnabled(true);
 
@@ -49,16 +57,12 @@ public class ClubsFragment extends Fragment {
             }
         });
 
-
-
-
         clubsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-           //parent is the list view, view is the clicked item, position is view's index, id
+            //parent is the list view, view is the clicked item, position is view's index, id
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //TODO:
-                Toast.makeText(getActivity(), "This will open the selected club homepage.", Toast.LENGTH_LONG).show();
-
+                //TODO: open the club home page view
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ClubHomePageFragment()).addToBackStack(null).commit();
             }
         });
 
